76a983fb884e37f1372bee793ce306a6c751de8d
Angel17x <58637028+Angel17x@users.noreply.github.com>
2021-04-29 14:24:23 +0000

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import fade from './modules/fadeScroll.js'
+
+
+new fade({
+    tag: '.fade_left',
+    direction: 'left',
+    overflow: true
+})
+
+new fade({
+    tag: '.fade_right',
+    direction: 'right',
+    overflow: true
+})
+
+new fade({
+    tag: '.fade_up',
+    direction: 'up',
+    overflow: true
+})
+
+new fade({
+    tag: '.fade_down',
+    direction: 'down',
+    overflow: true
+})
